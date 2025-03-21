bd46b45896ca2acbb1a4dd438f9644f4ebcf0772
Sam-Orion <121442461+Sam-Orion@users.noreply.github.com>
2025-03-22 00:14:02 +0530

    Added handler body

--- index.js
+++ index.js
@@ -3,19 +3,39 @@ const express = require('express');
 const app = new express();
 
 app.get("/sum", function(req,res) {
+    const a = req.query.a;
+    const b = req.query.b;
 
+    res.json({
+        ans: a + b
+    })
 });
 
 app.get("/multiply", function(req,res) {
+    const a = req.query.a;
+    const b = req.query.b;
 
+    res.json({
+        ans: a * b
+    })
 });
 
 app.get("/divide", function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
 
+    res.json({
+        ans: a / b
+    })
 });
 
 app.get("/subtract", function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
 
+    res.json({
+        ans: a - b
+    })
 });
 
 app.listen(3000);
