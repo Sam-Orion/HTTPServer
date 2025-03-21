38f2064218d421b3c3d5d754732f30ac33191f9c
Sam-Orion <121442461+Sam-Orion@users.noreply.github.com>
2025-03-22 00:08:49 +0530

    Added the endpoints

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const express = require('express');
+
+const app = new express();
+
+app.get("/sum", function(req,res) {
+
+});
+
+app.get("/multiply", function(req,res) {
+
+});
+
+app.get("/divide", function(req, res) {
+
+});
+
+app.get("/subtract", function(req, res) {
+
+});
+
+app.listen(3000);
